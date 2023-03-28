3a44646a23be02b4c0d46a4d7eafc7be8bb1d978
Kahu Mulligan <mulligkahu@myvuw.ac.nz>
2023-03-28 22:33:38 +1300

    edited an old assignment i did to make my student find where the bugs are with unit testing

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module chess {
+	exports swen221.chessview.moves;
+	exports swen221.tests;
+	exports swen221.chessview;
+	exports swen221.chessview.pieces;
+
+	requires junit;
+	requires org.junit.jupiter.api;
+}
